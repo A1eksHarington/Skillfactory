fa27335a00d7d2a44367474c90d48a052ecbd9b9
A1eksHarington <156945490+A1eksHarington@users.noreply.github.com>
2024-03-10 14:54:53 +0300

    Кортежи2

--- Модуль 13.Кортежи:Массивы:Коллекции.playground/Contents.swift
+++ Модуль 13.Кортежи:Массивы:Коллекции.playground/Contents.swift
@@ -6,3 +6,16 @@ let player : (String, String , Int) = ("Leonel", "Messi" ,9)
 player.0
 player.1
 player.2
+
+var player2 : (String, String , Int) = ("Alex", "Kharitonov" ,7)
+player2.0 = "Elza"
+player2.1 = "Kykla"
+player2.2 = 11
+
+print(player)
+print(player2)
+
+// Безымянный кортеж
+let tupleOne = (1.24, 42.3)
+// Названный кортеж
+let tupleTwo = (score: 32.2, total: 134)
