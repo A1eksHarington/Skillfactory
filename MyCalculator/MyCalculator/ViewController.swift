02485d063117939fd65ae36917c7210d0db8d61c
A1eksHarington <156945490+A1eksHarington@users.noreply.github.com>
2024-03-28 21:28:09 +0300

    Calculator

---
+++ MyCalculator/MyCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyCalculator
+//
+//  Created by Александр Харитонов on 28.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
